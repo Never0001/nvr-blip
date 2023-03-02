a0fbd9693c378bd1bfc75d21e7593bbe9f7dbfe3
Never0001 <97311722+Never0001@users.noreply.github.com>
2023-03-02 17:17:05 +0300

    Update config.lua

--- nvr-blip/config.lua
+++ nvr-blip/config.lua
@@ -2,6 +2,6 @@ Config = {}
 
 Config.blipcoords={
 
-    {name="Örnek Blip",    color=5, scale=0.8, id=106, x=-1197.24, y=-900.21, z=44.96}, -- Karpuz toplama noktası
+    {name="Örnek Blip",    color=5, scale=0.8, id=106, x=-1197.24, y=-900.21, z=44.96}, -- blup
    
   }
